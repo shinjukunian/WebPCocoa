412af2ecf2870cef015dcdd4cfebc570bb58a8be
Morten Bertz <morten@telethon.jp>
2016-09-13 18:20:47 +0900

    Initial Commit

---
+++ WebP/WebP.h
@@ -0,0 +1,19 @@
+//
+//  WebP.h
+//  WebP
+//
+//  Created by Morten Bertz on 9/13/16.
+//  Copyright © 2016 telethon k.k. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WebP.
+FOUNDATION_EXPORT double WebPVersionNumber;
+
+//! Project version string for WebP.
+FOUNDATION_EXPORT const unsigned char WebPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebP/PublicHeader.h>
+
+
